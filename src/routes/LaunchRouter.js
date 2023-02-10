f7ab2457f7cb741ad8ddc3f8a9524d8dbcc9820e
Angelo Bordin <angelombordin@gmail.com>
2023-02-09 23:26:53 -0300

    create launch route

---
+++ src/routes/LaunchRouter.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import {LaunchController} from '../controllers/LaunchController.js';
+
+export const LaunchRouter = Router();
+
+// LaunchRouter.get('/launch', LaunchController.getEventList);
+// LaunchRouter.get('/launch/:id', LaunchController.getEventById);
+// LaunchRouter.post('/launch', LaunchController.insertEvent);
+// LaunchRouter.put('/launch/:id', LaunchController.updateEvent);
+// LaunchRouter.delete('/launch/:id', LaunchController.deleteEvent);
